af3997c4e88e95b866dd30f0d23c013ffbacd169
Edmilson Pereira da Silva Junior <edmilsonjr.1@gmail.com>
2022-08-01 22:13:22 -0300

    tela cad solicitacao

--- src/screens/CadSolicitacao.js
+++ src/screens/CadSolicitacao.js
@@ -1,12 +1,24 @@
-import { Box, Center, CheckIcon, Select, View } from 'native-base';
-import React from 'react';
+import {
+  Box,
+  Button,
+  CheckIcon,
+  Input,
+  Select,
+  TextArea,
+  VStack,
+} from 'native-base';
+import React, { useState } from 'react';
 
 const CadSolicitacao = () => {
-  const [categoria, setCategoria] = React.useState('');
-  const [service, setService] = React.useState('');
+  const [categoria, setCategoria] = useState('');
+  const [service, setService] = useState('');
+  const [urgencia, setUrgencia] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [endereco, setEndereco] = useState('');
+  const [telefone, setTelefone] = useState('');
   return (
-    <Center>
-      <Box w="3/4" maxW="300">
+    <VStack space={4} alignItems="center" safeArea>
+      <Box w="90%">
         <Select
           selectedValue={categoria}
           minWidth="200"
@@ -16,37 +28,83 @@ const CadSolicitacao = () => {
             bg: 'teal.600',
             endIcon: <CheckIcon size="5" />,
           }}
-          mt={1}
-          onValueChange={(itemValue) => setService(itemValue)}
+          onValueChange={(itemValue) => setCategoria(itemValue)}
         >
           <Select.Item label="teste" value="teste" />
         </Select>
       </Box>
-      if (categoria != (''))
-      {
-        <Select
-          selectedValue={service}
-          minWidth="200"
-          accessibilityLabel="Escolha o Tipo de Serviço"
-          placeholder="Escolha o Tipo de Serviço"
-          _selectedItem={{
-            bg: 'teal.600',
-            endIcon: <CheckIcon size="5" />,
-          }}
-          mt={1}
-          onValueChange={(itemValue) => setService(itemValue)}
-        >
-          <Select.Item label="teste" value="teste" />
-        </Select>
-      }
-    </Center>
-  );
-  
+      {categoria != '' && (
+        <Box w="90%">
+          <Select
+            selectedValue={service}
+            minWidth="200"
+            accessibilityLabel="Escolha o Tipo de Serviço"
+            placeholder="Escolha o Tipo de Serviço"
+            _selectedItem={{
+              bg: 'teal.600',
+              endIcon: <CheckIcon size="5" />,
+            }}
+            onValueChange={(itemValue) => setService(itemValue)}
+          >
+            <Select.Item label="teste" value="teste" />
+          </Select>
+        </Box>
+      )}
+      {service != '' && (
+        <Box w="90%">
+          <Select
+            selectedValue={urgencia}
+            minWidth="200"
+            accessibilityLabel="Escolha o Tipo de Serviço"
+            placeholder="Escolha o Tipo de Serviço"
+            _selectedItem={{
+              bg: 'teal.600',
+              endIcon: <CheckIcon size="5" />,
+            }}
+            onValueChange={(itemValue) => setUrgencia(itemValue)}
+          >
+            <Select.Item label="teste" value="teste" />
+          </Select>
+        </Box>
+      )}
 
-  return (
-    <View>
-      <Text>categoria tipo urgencia Descrição endereço contato</Text>
-    </View>
+      {urgencia != '' && (
+        <Box>
+          <TextArea
+            h={20}
+            placeholder="Text Area Placeholder"
+            w="90%"
+            value={descricao}
+            onChangeText={setDescricao}
+          />
+        </Box>
+      )}
+      {descricao != '' && (
+        <Box alignItems="center">
+          <Input
+            placeholder="Digite seu Endereço"
+            w="90%"
+            value={endereco}
+            onChangeText={setEndereco}
+          />
+        </Box>
+      )}
+      {endereco != '' && (
+        <Box alignItems="center">
+          <Input
+            placeholder="Digite seu telefone para contato"
+            w="90%"
+            onChangeText={setTelefone}
+            value={telefone}
+          />
+        </Box>
+      )}
+      {telefone != '' && (
+        <Box>
+          <Button onPress={() => console.log('hello world')}>Click Me</Button>
+        </Box>
+      )}
+    </VStack>
   );
 };
 
